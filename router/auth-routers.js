0e87d8865bd7be9ac76a973d81166fd7bb192004
Gajanan Shinde <gajushinde8046@gmail.com>
2026-04-01 16:32:16 +0530

    indenting

--- router/auth-routers.js
+++ router/auth-routers.js
@@ -1,5 +1,5 @@
 const express =require("express")
-const  authController  = require("../controller/auth-controller")
+const authController  = require("../controller/auth-controller")
 const router = express.Router()
 const {registerSchema, loginSchema,} = require("../validators/validators")
 const validate = require("../middleware/validate-middleware")
